e3bbe4262d55dc7607cf3fc7c6a0d4ac5a2f5b55
caokun <you@example.com>
2022-06-21 22:12:13 +0800

    antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import { Button,Rate } from 'antd'
+import { Button } from 'antd'
+import Layout from "./Layout";
 
 const Home: NextPage = () => {
   return (
     <div>
-        <Link href="/home">
-          <a>Back to home</a>
-        </Link>
-        <Button type="primary">这是一个按钮</Button>
-        <Rate />
+        <Layout/>
     </div>
   )
 }
